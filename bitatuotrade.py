c5a402e923cfbb4d7f4cde5071cd06709f948739
전성주 <76582596+sungzoochon@users.noreply.github.com>
2022-02-25 20:00:35 +0900

    Update bitatuotrade.py

--- bitatuotrade.py
+++ bitatuotrade.py
@@ -6,8 +6,8 @@ import numpy as np
 
 access = "dNlYFPozFWnpa6gtT7OuOjLFbx7iD65qCkWVxxTg"
 secret = "nzhg0ah7Lh3XX2nKlUxBY904RBoSbpBHMrf8pGrZ"
-lis = ["KRW-BTC", "KRW-ETH", "KRW-BCH", "KRW-AAVE","KRW-LTC","KRW-SOL","KRW-BSV","KRW-AVAX","KRW-AXS","KRW-STRK","KRW-BTG","KRW-ETC","KRW-ATOM","KRW-NEO","KRW-DOT","KRW-REP","KRW-LINK"]
-les = ["BTC","ETH","BCH","AAVE","LTC","SOL","BSV","AVAX","AXS","STRK","BTG","ETC","ATOM","NEO","DOT","REP","LINK"]
+lis = ["KRW-BTC", "KRW-ETH", "KRW-BCH", "KRW-AAVE","KRW-LTC","KRW-SOL","KRW-BSV","KRW-AVAX","KRW-AXS","KRW-STRK","KRW-BTG","KRW-ETC","KRW-ATOM","KRW-NEO","KRW-DOT","KRW-REP","KRW-LINK","KRW-WAVES","KRW-NEAR","KRW-QTUM","KRW-FLOW","KRW-WEMIX","KRW-GAS","KRW-SBD","KRW-OMG","KRW-TON","KRW-XTZ","KRW-SAND","KRW-KAVA","KRW-THETA","KRW-MANA","KRW-AQT","KRW-LSK","KRW-EOS","KRW-CBK","KRW-SRM","KRW-KNC","KRW-DAWN","KRW-MATIC","KRW-ENJ","KRW-1INCH","KRW-MTL","KRW-SXP","KRW-STX","KRW-BORA","KRW-STORJ","KRW-STRAX","KRW-ADA","KRW-PLA","KRW-HIVE","KRW-ALGO","KRW-ARK","KRW-MILK","KRW-ONG","KRW-IOTA","KRW-PUNDIX","KRW-XRP","KRW-BAT","KRW-HUNT","KRW-ICX","KRW-GRS","KRW-GRS","KRW-POWR","KRW-ONT","KRW-NU","KRW-CRO","KRW-GLM","KRW-POLY","KRW-ELF","KRW-STEEM","KRW-WAXP","KRW-CVC","KRW-HUM","KRW-HBAR","KRW-XLM","KRW-ARDR","KRW-AERGO","KRW-CHZ","KRW-TFUEL","KRW-MOC","KRW-DOGE","KRW-UPP","KRW-XEM","KRW-FCT2","KRW-DKA","KRW-STPT","KRW-LOOM","KRW-META","KRW-TRX","KRW-ORBS","KRW-ANKR","KRW-SNT","KRW-VET","KRW-JST","KRW-ZIL","KRW-SSX","KRW-MED","KRW-IOST"]
+les = ["BTC","ETH","BCH","AAVE","LTC","SOL","BSV","AVAX","AXS","STRK","BTG","ETC","ATOM","NEO","DOT","REP","LINK","WAVES","NEAR","QTUM","FLOW","WEMIX","GAS","SBD","OMG","TON","XTZ","SAND","KAVA","KRW-THETA","MANA","AQT","LSK","EOS","CBK","SRM","KNC","DAWN","MATIC","ENJ","1INCH","MTL","SXP","STX","KRW-BORA","STORJ","STRAX","ADA","PLA","HIVE","ALGO","ARK","MILK","ONG","IOTA","PUNDIX","XRP","BAT","HUNT","ICX","GRS","GRS","POWR","ONT","NU","CRO","GLM","POLY","ELF","STEEM","WAXP","CVC","HUM","HBAR","XLM","ARDR","AERGO","CHZ","TFUEL","MOC","DOGE","UPP","XEM","FCT2","DKA","STPT","LOOM","META","TRX","ORBS","ANKR","SNT","VET","JST","ZIL","SSX","MED","IOST"]
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
@@ -47,7 +47,7 @@ shift = 0
 fin = []
 while True:
     try: 
-      for i in range(1,17):
+      for i in range(1,98):
         if shift == 0:
          coin = lis[i]
          coini = les[i]
@@ -86,18 +86,18 @@ while True:
             if shift == 1:
                
                if current_price < buy_price * 0.97: 
-                    upbit.sell_market_order(coin, btc*0.9995)
+                    upbit.sell_market_order(coin, btc)
                     shift = 0
                     print("하락이다 돔황차!!!!!")
                if current_price > buy_price * 1.20:
-                    upbit.sell_market_order(coin, btc*0.9995)
+                    upbit.sell_market_order(coin, btc)
                 
                     shift = 0
                     
                     print("고점이다 돔황차!!!!!")
         else:
             btc = get_balance(coini)
-            upbit.sell_market_order(coin, btc*0.9995)
+            upbit.sell_market_order(coin, btc)
             shift = 0
             fin = []
             print("다들 돔황차!!!!!")
