3e82079fd8c21a9a9b5e4f51d7fcf1ddd90a1245
전성주 <76582596+sungzoochon@users.noreply.github.com>
2022-03-01 13:55:07 +0900

    Update bitatuotrade.py

--- bitatuotrade.py
+++ bitatuotrade.py
@@ -1,11 +1,22 @@
 import time
 import pyupbit
 import datetime
+import numpy as np
 
 access = "dNlYFPozFWnpa6gtT7OuOjLFbx7iD65qCkWVxxTg"
 secret = "nzhg0ah7Lh3XX2nKlUxBY904RBoSbpBHMrf8pGrZ"
-lis = ["KRW-BTC", "KRW-ETH", "KRW-BCH", "KRW-AAVE","KRW-LTC","KRW-SOL","KRW-BSV","KRW-AVAX","KRW-AXS","KRW-STRK","KRW-BTG","KRW-ETC","KRW-ATOM","KRW-NEO","KRW-DOT","KRW-REP","KRW-LINK","KRW-WAVES","KRW-NEAR","KRW-QTUM","KRW-FLOW","KRW-WEMIX","KRW-GAS","KRW-SBD","KRW-OMG","KRW-TON","KRW-XTZ","KRW-SAND","KRW-KAVA","KRW-THETA","KRW-MANA","KRW-AQT","KRW-LSK","KRW-EOS","KRW-CBK","KRW-SRM","KRW-KNC","KRW-DAWN","KRW-MATIC","KRW-ENJ","KRW-1INCH","KRW-MTL","KRW-SXP","KRW-STX","KRW-BORA","KRW-STORJ","KRW-STRAX","KRW-ADA","KRW-PLA","KRW-HIVE","KRW-ALGO","KRW-ARK","KRW-MILK","KRW-ONG","KRW-IOTA","KRW-PUNDIX","KRW-XRP","KRW-BAT","KRW-HUNT","KRW-ICX","KRW-GRS","KRW-POWR","KRW-ONT","KRW-NU","KRW-CRO","KRW-GLM","KRW-POLY","KRW-ELF","KRW-STEEM","KRW-WAXP","KRW-CVC","KRW-HUM","KRW-HBAR","KRW-XLM","KRW-ARDR","KRW-AERGO","KRW-CHZ","KRW-TFUEL","KRW-MOC","KRW-DOGE","KRW-UPP","KRW-XEM","KRW-FCT2","KRW-DKA","KRW-STPT","KRW-LOOM","KRW-META","KRW-TRX","KRW-ORBS","KRW-ANKR","KRW-SNT","KRW-VET","KRW-JST","KRW-ZIL","KRW-SSX","KRW-MED","KRW-IOST"]
 les = ["BTC","ETH","BCH","AAVE","LTC","SOL","BSV","AVAX","AXS","STRK","BTG","ETC","ATOM","NEO","DOT","REP","LINK","WAVES","NEAR","QTUM","FLOW","WEMIX","GAS","SBD","OMG","TON","XTZ","SAND","KAVA","KRW-THETA","MANA","AQT","LSK","EOS","CBK","SRM","KNC","DAWN","MATIC","ENJ","1INCH","MTL","SXP","STX","KRW-BORA","STORJ","STRAX","ADA","PLA","HIVE","ALGO","ARK","MILK","ONG","IOTA","PUNDIX","XRP","BAT","HUNT","ICX","GRS","POWR","ONT","NU","CRO","GLM","POLY","ELF","STEEM","WAXP","CVC","HUM","HBAR","XLM","ARDR","AERGO","CHZ","TFUEL","MOC","DOGE","UPP","XEM","FCT2","DKA","STPT","LOOM","META","TRX","ORBS","ANKR","SNT","VET","JST","ZIL","SSX","MED","IOST"]
+def get_ror(k=0.5,coin = "KRW-BTC"):
+    df = pyupbit.get_ohlcv(coin, count=4)
+    df['range'] = (df['high'] - df['low']) * k
+    df['target'] = df['open'] + df['range'].shift(1)
+
+    df['ror'] = np.where(df['high'] > df['target'],
+                         df['close'] / df['target'],
+                         1)
+
+    ror = df['ror'].cumprod()[-2]
+    return ror
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
@@ -38,46 +49,57 @@ upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 coin = ""
 coini = ""
-shift = 0
+shift = 1
 buylist = []
-# 자동매매 시작
+res = 0
+buyprice = 0
+i = len(les) - 1
 while True:
     try:
-     for i in range(1,len(les)):    
+     if i >= 0:    
          now = datetime.datetime.now()
          if shift == 0:
-          coin = lis[i]
+          coin = "KRW-"+les[i]
           coini = les[i]
-          start_time = get_start_time(coin)
-          end_time = start_time + datetime.timedelta(days=1)
-         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price(coin, 0.5)
-            current_price = get_current_price(coin)
+         start_time = get_start_time(coin)
+         end_time = start_time + datetime.timedelta(days=1)
+         if start_time < now < end_time - datetime.timedelta(seconds=5):
             if shift == 0:
-             if target_price < current_price:
+             ror = []   
+             for k in np.arange(0.1, 1.0, 0.1):
+              ror.append(get_ror(k,coin))
+             M = max(ror)
+             res = (ror.index(M) + 1)/10
+             print(res,coin) 
+             target_price = get_target_price(coin,res)
+             current_price = get_current_price(coin)
+             if coin not in buylist:   
+              if target_price < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
                     upbit.buy_market_order(coin, krw*0.9995)
                     shift = 1
                     buylist.append(coin)
                     buy_price = current_price
-                    print("buybuybuybuy")
+                    print("상승이다 풀매수!!!!")
             if shift == 1:
-             if buy_price*0.97 > current_price:
+             if buy_price*0.955 > current_price:
                 btc = get_balance(coini)
-                upbit.sell_market_order(coin, btc*0.9995)
-                shift = 0
-             if buy_price*1.25 < current_price:
+                upbit.sell_market_order(coin, btc)
+                print("하락장이다 돔황차!!!!")
+             if buy_price*1.15 < current_price:
                 btc = get_balance(coini)
-                upbit.sell_market_order(coin, btc*0.9995)
-                shift = 0
+                upbit.sell_market_order(coin, btc)
+                print("익절이다 돔황차!!!!")
          else:
             btc = get_balance(coini)
             if btc > 0.00008:
-                upbit.sell_market_order(coin, btc*0.9995)
+                upbit.sell_market_order(coin, btc)
                 shift = 0
-                print("sellsellsellsell")
-         time.sleep(0.5)
+                buylist = []
+                print("9시다 돔황차!!!!") 
+         i = i - 1   
+     else:
+         i = len(les) - 1      
     except Exception as e:
-        print(e)
-        time.sleep(1)
+        pass
